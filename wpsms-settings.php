10a5b0405f33ff8d4e2fc67d9716e3ee7dc8cbff
Al Amin <linkers4me@gmail.com>
2025-07-09 02:03:04 +0600

    FixError: Ensure safe access to API options in settings form

--- wpsms-settings.php
+++ wpsms-settings.php
@@ -11,15 +11,15 @@ $wpsms_options = get_option('wpsms_api_options');
         <table class="form-table">
             <tr>
                 <th scope="row">API Username</th>
-                <td><input type="text" name="wpsms-api-user" id="wpsms-api-username" value="<?= $wpsms_options['api_username']?>" required/></td>
+                <td><input type="text" name="wpsms-api-user" id="wpsms-api-username" value="<?= $wpsms_options['api_username'] ?? '' ?>" required/></td>
             </tr>
             <tr>
                 <th scope="row">API KEY</th>
-                <td><input type="text" name="wpsms-api-key" id="wpsms-api-key" value="<?= $wpsms_options['api_key']?>" required/></td>
+                <td><input type="text" name="wpsms-api-key" id="wpsms-api-key" value="<?= $wpsms_options['api_key'] ?? '' ?>" required/></td>
             </tr>
             <tr>
                 <th>Enable SMS</th>
-                <td><input type="checkbox" name="wpsms-enable-sms" id="toggle-sms" <?php checked($wpsms_options['enable_sms'], "1") ?>></td>
+                <td><input type="checkbox" name="wpsms-enable-sms" id="toggle-sms" <?php checked($wpsms_options['enable_sms'] ?? '' , "1") ?>></td>
             </tr>
             <tr>
                 <input type="hidden" name="action" value="wpsms_api_options">
